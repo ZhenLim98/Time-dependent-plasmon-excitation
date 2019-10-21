a4286334821540d39f9eb294ddd3af83434a095f
ZhenLim98 <40392819+ZhenLim98@users.noreply.github.com>
2019-10-21 12:41:59 +0100

    main2
    Week 2

--- main.m
+++ main.m
@@ -10,32 +10,32 @@ e0 = 8.85e-12;
 fsConst = 1/137;
 e1 = 1;
 e2 = 1;
-eF = 1*ee;%2*ee;
-gamma = 1e12;
+eF = 0.6*ee;%2*ee;
+gamma = 0.5e11;
 
-alphaF = 0.3;%0.15;
+alphaF = 0.2;%0.15;
 if alphaF > 0.5
     error('Invalid alpha');
 end
 
-L = 1e-6; %1e-2
+L = 1; %1e-2
 g = pi/L;%0.001*pi/L;
-Omega = 2*pi*1e6; % 2*pi*1e12;
+Omega = 2*pi*3e12; % 2*pi*1e12;
 
 Ng = 20;
 
-nqPoints = 1;
-thetamin = 0.001;
-thetamax = 0.001;
-thetaVec = linspace(thetamin,thetamax,nqPoints);
+nqPoints = 100;
+% thetamin = 499 * pi/1000;
+% thetamax = 499 * pi/1000;
+% thetaVec = linspace(thetamin,thetamax,nqPoints);
 
-qmin = 0.0001*g;%0.1e6;
-qmax = 0.0001*g;%0.1e6;
+qmin = 0.001e6;%0.1e6;
+qmax = 10e6;%0.1e6;
 qVec = linspace(qmin,qmax,nqPoints);
 
 nOmPoints = 500;
-omin = qmax*cl;
-omax = 2*pi*6e13;%2*pi*10.001e12;
+omin = 0 * 2*pi*1*1e12;
+omax = +10*pi*1*1e12;%2*pi*10.001e12;
 omVec = linspace(omin,omax,nOmPoints);
 
 ttVec = zeros(nOmPoints,nqPoints);
@@ -45,12 +45,13 @@ recVecs = -Ng:Ng;
 
 figure() % %figure()
 
-for jq = 1:length(thetaVec)
+for jq = 1:length(qVec)
     for jom = 1:length(omVec)
         om = omVec(jom);
         k0 = sqrt(e2)*om/cl;
-%         q  = k0*sin(thetaVec(jq));
         q = qVec(jq);
+        %q  = k0*sin(thetaVec(jq));
+%        q = qVec(jq);
         kz = (q<=k0)*sqrt(k0^2-q^2) + (q>k0)*1i*sqrt(q^2-k0^2);%-(q>k0)*1i*sqrt(q^2-k0^2); % k0*cos(theta);
         if imag(kz)~=0
             warning('evanescent source used')
@@ -62,13 +63,13 @@ for jq = 1:length(thetaVec)
 
         [transmitE,kap1n,kap2n] = trans_calc(q,om,g,Omega,e1,e2,Ng,alphaF,sou);        
         reflE = 1-transmitE(Ng+1);
-        
         ttVec(jom,jq) = e1/e2*kz./abs(kap1n(Ng+1)).*abs(transmitE(Ng+1)).^2; % n=0 only
         rrVec(jom,jq) = (kz./abs(kap1n(Ng+1))).*abs(reflE).^2;
     end
-    plot(omVec/2/pi/1e12,abs(ttVec(:,jq)),'LineWidth',1.5);
-    hold on
+%     plot(omVec/2/pi/1e12, q,abs(ttVec(:,jq)),'LineWidth',1.5);
+%     hold on
 end
+contourf(qVec/1e6,omVec/2/pi/1e12,log(abs(ttVec)),200,'LineColor','none')
 
 %ylim([0,1])
 %xlim([0,10])
@@ -84,7 +85,7 @@ end
 % colorbar
 % ylabel('f')
 % xlabel(' \theta   ')
-% 
+%
 % figure()
 % contourf(thetaVec,omVec,abs(rrVec),nlayers,'LineColor', 'none')
 % % hold on
@@ -93,9 +94,9 @@ end
 % title('Reflectance')
 % ylabel('$$ \omega $$')
 % xlabel('q')
-% 
+%
 % absVec = 1-ttVec-rrVec;
-% 
+%
 % figure()
 % contourf(thetaVec,omVec,abs(absVec),nlayers,'LineColor', 'none')
 % % hold on
