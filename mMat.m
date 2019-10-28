24ac174b331e562ceb9db548a0b821a3717dfce1
ZhenLim98 <40392819+ZhenLim98@users.noreply.github.com>
2019-10-28 15:25:29 +0000

    NEW FORMALISM (WIP)

--- mMat.m
+++ mMat.m
@@ -8,26 +8,35 @@ kap1n = kapn(q,om,g,Omega,e1,Ng);
 kap2n = kapn(q,om,g,Omega,e2,Ng);
 %produces the k vectors
 
-mod_conduc = sigmaDrude + alphaF / 2 * cos(Omega);
+out = zeros(2 * Ng + 1);
+    for i = 1: Ng
+        %constructing vector of conductivity frequency coefficients
+        sig = sigma2(om + recVecs * Omega, om, Omega, Ng)./ ...
+              e0 .*(om + (recVecs).* Omega);
+        %picks out an element in the conductivity vector to construct
+        %diagonal
+        sig_u = sig(Ng + 1 + i);%sigma2(om-i* Omega,om, Omega, Ng);
+        sig_l = sig(Ng + 1 - i);%sigma2(om+i* Omega,om, Omega, Ng);
+        
+        %constructing diagonals
+        sig_u = sig_u * (ones(1,2*Ng + 1 - i));
+        sig_l = sig_l * (ones(1,2*Ng + 1 - i));
+        upper_diag = 1i.*sig_u*alphaF; % upper diagonal;
+        lower_diag = 1i.*sig_l*alphaF; %lower diagonal;
+        out = out +  diag( upper_diag,+i) + diag( lower_diag,-i);
+        %2N+1 dimension
+    end
+    %main diagonal
+out = out + diag(e1./kap1n + e2./kap2n + 1i * sigma2(om + recVecs*Omega, om, Omega, Ng)./(e0*(om + recVecs*Omega)),0);
 
-% out = zeros(2 * Ng + 1);
-%     for i = 1:1
-%         upper_diag = 1i*sigmaDrude(om+(recVecs((i + 1):end)-i)*Omega)*alphaF./ ...
-%                      (e0*(om+(recVecs((i + 1):end)-i)*Omega));
-%         lower_diag = 1i*sigmaDrude(om+(recVecs(1:(end-i))+i)*Omega)*alphaF./ ...
-%                      (e0*(om+(recVecs(1:(end-i))+i)*Omega)); % upper diagonal
-%         out = out +  diag( upper_diag,+i) + diag( lower_diag,-i);
-%         %2N+1 dimension
-%     end
-% out = out + diag(e1./kap1n + e2./kap2n + 1i * sigmaDrude(om + recVecs*Omega)./(e0*(om + recVecs*Omega)),0);
- 
-out = full(gallery('tridiag', ...
-            1i*sigmaDrude(om+(recVecs(2:end)-1)*Omega)*alphaF./ ...
-                (e0*(om+(recVecs(2:end)-1)*Omega)) ,... % lower diagonal
-            (e1./kap1n+e2./kap2n) + 1i*sigmaDrude(om+recVecs*Omega)./ ...
-                (e0*(om+recVecs*Omega)), ... % diagonal
-            1i*sigmaDrude(om+(recVecs(1:end-1)+1)*Omega)*alphaF./ ...
-                (e0*(om+(recVecs(1:end-1)+1)*Omega)) )); % upper diagonal
+%  
+% out = full(gallery('tridiag', ...
+%             1i*sigmaDrude(om+(recVecs(2:end)-1)*Omega)*alphaF./ ...
+%                 (e0*(om+(recVecs(2:end)-1)*Omega)) ,... % lower diagonal
+%             (e1./kap1n+e2./kap2n) + 1i*sigmaDrude(om+recVecs*Omega)./ ...
+%                 (e0*(om+recVecs*Omega)), ... % diagonal
+%             1i*sigmaDrude(om+(recVecs(1:end-1)+1)*Omega)*alphaF./ ...
+%                 (e0*(om+(recVecs(1:end-1)+1)*Omega)) )); % upper diagonal
 
 end
 %original
