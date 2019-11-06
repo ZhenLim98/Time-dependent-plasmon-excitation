f306fbcd103619fa55df75612400d5de94361b67
ZhenLim98 <40392819+ZhenLim98@users.noreply.github.com>
2019-11-06 13:32:02 +0000

    before directly changing the conductivity

--- lambda.m
+++ lambda.m
@@ -1,37 +1,31 @@
-function [ out ] = lambda2( om_vec,om, Omega, n_vec, Ng)
+function [ out ] = lambda( om_vec,om, Omega, n_vec, Ng)
 %conductivity coefficients for a conductivity function that:
 % starts at y1 for t = 0
 % reaches y2 for t = t1
 % goes back to y1 for t = t2
 %period length t2
 %y axis units is in sigmaDrude
-sigma_drude = sigmaDrude(om_vec);
-T = 2 * pi/Omega;
-y1 = 0.2;
-y2 = 0.8;
-t1 = 0.1;
-t2 = 2.1;
-grad1 = 6;
-grad2 = -0.6/2;
+T = 2.1e-12;
+y1 = 0.2 ;%* sigmaDrude(om_vec) ;
+y2 = 0.8 ;%* sigmaDrude(om_vec) ;
+t1 = 0.1e-12;
+exp_t = exp(-2* pi * 1i .* t1 .* n_vec ./ T) ;
+out = (y1-y2)./(2*pi*1i .* n_vec) .* (1 - 1/(T - t1) * (T - t1 .* exp_t));
+out = out + T.*T./(2 * pi .* n_vec .* n_vec .* t1 .* (T - t1)) .* (y1-y2).*(1-exp_t);
 
-out = sigma_drude./(2.*T) .* (y1 .* (exp( -1i .* n_vec )) - 1)./(-1i .* n_vec .* Omega) + ...
-                        grad1 .* sigma_drude./(2 .* T) .* (1./(n_vec .* n_vec .* Omega .* Omega))...
-                        .* (exp(-1i .* n_vec .* Omega .* t1) - 1) + ...
-                        grad1 .* sigma_drude./(2 .* T).*(t1.* exp(- n_vec .* Omega .* t1))/(-1i .* n_vec .* Omega));
-%first section of fourier expansion
+%Replaces singularities in the conductivity with the n = 0 fourier coefficient
+%change this
+nan = isnan(out);
+out(nan) =  (y1 + (y2-y1) * T/(2*t1));%(y1(nan) + (y2(nan)-y1(nan)) * T/(2*t1));
 
-out = out + sigma_drude./(2*T) * (y2)./(-1i .* n_vec .* Omega) ...
-                        .* (exp(-1i .* n_vec .* t2) - exp(-1i .* n_vec .* Omega .* t1))...
-                         + grad2 .*(sigma_drude./(2 * T))./(-1i .* n_vec .* Omega) ...
-                     .* (t2 .* exp(-1i .* n_vec .* Omega .* t2) - t1*(exp(-1i .* n_vec .* Omega .* t1)))...
-                     + (1./(n_vec .* n_vec .* Omega .* Omega).*(exp(-1i .* n_vec .* Omega .* t2) - (exp(-1i .* n_vec .* Omega .* t1))));
-%second section of the fourier expansion
-%for the n = 0 fourier expansion coefficient.
-sigma_0 = sigmaDrude(om);
-out(Ng + 1) = sigma_0/(2 .* T) .* (y1 .* t1 + grad1 .* t1 .* t1 ./ 2)  ...
-                        + sigma_0/(2 .* T) .* ((t2 - t1) .* y2 + grad2./2 .* (t2 .* t2 - t1 .* t1) ); 
-                    
-%CONVERTING CONDUCTIVITY INTO DRUDE WEIGHT:
-% out = out * -1i * omega/c
+%%
+%gaussian modulation of the function in frequency space to justify sharp
+%cutoff
+g_sigma = 500;
+mu = 0;
+%68% of the content is within +- g sigma here
+gaussian =  1/sqrt (2*pi*g_sigma*g_sigma)  * exp(- (n_vec - mu) .* (n_vec-mu)./(2 * g_sigma * g_sigma));
+out = out .* gaussian;
+
 end
 
