4c993a32e7bf9d07165d28ea7d48ee7baa15c109
ZhenLim98 <40392819+ZhenLim98@users.noreply.github.com>
2019-11-15 14:16:25 +0000

    Add files via upload

--- disp_rel_full_ret.m
+++ disp_rel_full_ret.m
@@ -3,14 +3,14 @@ global hbar ee cl eF gamma e0
 
 % for Omega = 2*pi*linspace(0,0.4,5)*1e12
 
-hbar = 1.05e-34;
+hbar = 6.63e-34/(2*pi);
 ee = 1.6e-19;
 cl = 3e8;
 e0 = 8.85e-12;
 fsConst = 1/137;
 e1 = 1;
 e2 = 1;
-gamma = 1e11;
+gamma = 1e12;
 
 x = linspace(1,10,10);
 y = linspace(1,10,10);
@@ -21,15 +21,15 @@ y = linspace(1,10,10);
 % figure()
 % contourf(X,Y,Z)
 
-nom = 200;
-nk = 200;
+nom = 800;
+nk = 800;
 
 kVec = linspace(1e4,2e6,nk);
 omVec = 2*pi*linspace(1e11,10e12,nom);
 [K,OM] = meshgrid(kVec,omVec);
 
 figure()
-for eF = [2]*ee
+for eF = [1.5]*ee
     DrW = ee^2/pi/hbar^2*eF;
     disp_vis = e1./sqrt(K.^2-e1*OM.^2/cl^2) ...
         + e2./sqrt(K.^2-e2*OM.^2/cl^2) ...
